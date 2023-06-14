b23eb4c4a30d561c5bddab6ef82ecfcb6d67afe5
Didrik Mikkelsen <didrikmikkelsen.92@gmail.com>
2023-06-14 15:14:11 +0200

    Addes files and some javascript and some HTML structure

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const Player = (name, age) => {
+    return { name, age }
+    };
+
+
+const Tom = Player("Tom", 10);
+
+//console.log(Tom);
+
+
+
+const Gameboard = (() => {
+    const gameboard = [];
+    return {
+      gameboard
+    };
+  })();
