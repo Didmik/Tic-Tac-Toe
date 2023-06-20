31ef69249b2504b089e2df6645f3a86004d60607
Didrik Mikkelsen <didrikmikkelsen.92@gmail.com>
2023-06-20 12:08:06 +0200

    Almost finished the project. Needs som polish

--- script.js
+++ script.js
@@ -19,7 +19,16 @@ const Gameboard = (() => {
     
     const displayController = () => { 
 
-         let buttons = document.querySelectorAll(".box");
+        let submitButton = document.querySelector("#submit");
+
+        let playerStartButton = document.querySelector(".playerStart");
+
+        let playerX = document.querySelector("#player1");
+        let playerO = document.querySelector("#player2");
+
+        let gameCont = document.querySelector(".gameContainer");
+
+        let buttons = document.querySelectorAll(".box");
 
          let button0 = document.querySelector("#nil");
          let button1 = document.querySelector("#one");
@@ -31,7 +40,17 @@ const Gameboard = (() => {
          let button7 = document.querySelector("#seven");
          let button8 = document.querySelector("#eight");
 
+         let endMessage = document.querySelector(".endgameMessage");
+
          let restartButton = document.querySelector(".restart");
+
+
+         submitButton.addEventListener("click", () => {
+            playerStartButton.innerHTML = playerX.value.bold() + " starts with X. " 
+            + playerO.value.bold() + " follows with O"; 
+
+            gameCont.style.visibility = "visible";  
+         })
 
          
             restartButton.addEventListener("click", () => {
@@ -39,26 +58,51 @@ const Gameboard = (() => {
                     ev.textContent = "";
                     ev.disabled = false;
                     gameboard = ["X","O","X","O","X","O","X","O","X"]
+                    playerStartButton.textContent = "";
+                    endMessage.textContent = "";
+                    gameCont.style.visibility = "hidden"; 
                 }) 
             })
             
          
 
-
          buttons.forEach(button => {
             button.addEventListener("click", (e) => {
                
-                    //console.log(gameboard)
                 if (gameboard.length > 0) { 
                    e.target.textContent = gameboard.splice(0, 1);  
                    button.disabled = true;
                 }
 
                 if((button0.textContent === "X" && button1.textContent === "X" && button2.textContent === "X") ||
-                (button0.textContent === "O" && button1.textContent === "O" && button2.textContent === "O")) {
-                    console.log("You Won");
+                   (button3.textContent === "X" && button4.textContent === "X" && button5.textContent === "X") ||
+                   (button6.textContent === "X" && button7.textContent === "X" && button8.textContent === "X") ||
+                   (button0.textContent === "X" && button3.textContent === "X" && button6.textContent === "X") ||
+                   (button1.textContent === "X" && button4.textContent === "X" && button7.textContent === "X") ||
+                   (button2.textContent === "X" && button5.textContent === "X" && button8.textContent === "X") ||
+                   (button0.textContent === "X" && button4.textContent === "X" && button8.textContent === "X") ||
+                   (button2.textContent === "X" && button4.textContent === "X" && button6.textContent === "X") ) 
+                   {
+                    //console.log("Player X Won");
+                    endMessage.textContent = playerX.value + " Won. Congratulations!";
+                    gameboard = [];
                 }
-                 
+
+        else if  ((button0.textContent === "O" && button1.textContent === "O" && button2.textContent === "O") ||
+                  (button3.textContent === "O" && button4.textContent === "O" && button5.textContent === "O") ||
+                  (button6.textContent === "O" && button7.textContent === "O" && button8.textContent === "O") ||
+                  (button0.textContent === "O" && button3.textContent === "O" && button6.textContent === "O") ||
+                  (button1.textContent === "O" && button4.textContent === "O" && button7.textContent === "O") ||
+                  (button2.textContent === "O" && button5.textContent === "O" && button8.textContent === "O") ||
+                  (button0.textContent === "O" && button4.textContent === "O" && button8.textContent === "O") ||
+                  (button2.textContent === "O" && button4.textContent === "O" && button6.textContent === "O") ) 
+                  {
+                    //console.log("Player O Won");
+                    endMessage.textContent = playerO.value + " Won. Congratulations!";
+                    gameboard = [];
+                }
+
+           
              
              })
         });
